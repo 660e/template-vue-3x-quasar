45785c707821425f5d14c33eae603057e24bfdf0
660e <solunar@vip.qq.com>
2022-06-06 13:01:55 +0800

    update layouts

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -3,13 +3,22 @@
     <q-header>
       <q-toolbar>
         <q-btn @click="expand" icon="menu" dense flat round />
-        <q-toolbar-title>Quasar App</q-toolbar-title>
+        <q-toolbar-title></q-toolbar-title>
         <div class="q-gutter-x-xs">
           <q-btn :icon="$q.dark.isActive ? 'brightness_5' : 'brightness_4'" @click="$q.dark.toggle()" dense flat round />
         </div>
       </q-toolbar>
     </q-header>
-    <q-drawer v-model="expanded" bordered show-if-above></q-drawer>
+    <q-drawer v-model="expanded" bordered show-if-above>
+      <div class="bg-grey-3 flex flex-center" style="height: 50px">
+        <img src="~assets/logotype.svg" style="height: 40%" />
+      </div>
+      <q-list>
+        <q-item v-for="l in links" :key="l.path" :to="{ name: l.name }" clickable v-ripple>
+          <q-item-section class="text-subtitle1 text-right">{{ l.path }}</q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
     <q-page-container>
       <q-page :style-fn="tweak">
         <q-scroll-area class="fit">
@@ -22,6 +31,7 @@
 
 <script>
 import { defineComponent, ref } from 'vue';
+import { routes } from '@/router';
 
 export default defineComponent({
   name: 'app-layout',
@@ -32,6 +42,8 @@ export default defineComponent({
       expanded.value = !expanded.value;
     };
 
+    const links = routes.find(r => r.path === '/demo').children;
+
     const tweak = (offset, height) => {
       return { height: `${height - offset}px` };
     };
@@ -39,6 +51,7 @@ export default defineComponent({
     return {
       expanded,
       expand,
+      links,
       tweak
     };
   }
