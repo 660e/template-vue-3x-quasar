8654b0ab466c0cf6229b2d4db7e80e354917e8b1
660e <solunar@vip.qq.com>
2022-06-07 13:52:35 +0800

    update form

--- src/pages/demo/form.vue
+++ src/pages/demo/form.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="q-pa-md q-gutter-x-md row">
     <div style="width: 300px">
-      <q-input v-model="internal.name" :rules="[rules.required]" label="name" dense outlined />
+      <div class="q-pb-sm">Internal</div>
+      <q-input v-model="internal.name" :rules="[internalRules.required]" label="name" dense outlined />
     </div>
     <div style="width: 300px">
-      <q-input v-model="external.name" label="name" dense outlined />
+      <div class="q-pb-sm">External</div>
+      <q-input
+        v-model="external.name"
+        @blur="v$.name.$touch"
+        :error="v$.name.$error"
+        :error-message="v$.name.required.$message"
+        label="name"
+        dense
+        outlined
+      />
     </div>
+    <pre>{{ v$.name }}</pre>
   </div>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
+import { useVuelidate } from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
 
 export default defineComponent({
   name: 'demo-form',
@@ -19,18 +32,23 @@ export default defineComponent({
     const internal = ref({
       name: ''
     });
-    const rules = ref({
-      required: v => !!v || 'required'
-    });
+    const internalRules = {
+      required: v => !!v || 'Value is required'
+    };
 
     const external = ref({
       name: ''
     });
+    const externalRules = {
+      name: { required }
+    };
+    const v$ = useVuelidate(externalRules, external);
 
     return {
       internal,
-      rules,
-      external
+      internalRules,
+      external,
+      v$
     };
   }
 });
