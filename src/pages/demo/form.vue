064579fa575bc421d85b1b914ee03dcd3a56318a
660e <solunar@vip.qq.com>
2022-06-07 11:44:52 +0800

    update form

--- src/pages/demo/form.vue
+++ src/pages/demo/form.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="q-pa-md">
-    <q-form>form</q-form>
+  <div class="q-pa-md q-gutter-x-md row">
+    <div style="width: 300px">
+      <q-input v-model="internal.name" :rules="[rules.required]" label="name" dense outlined />
+    </div>
+    <div style="width: 300px">
+      <q-input v-model="external.name" label="name" dense outlined />
+    </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
   name: 'demo-form',
 
   setup() {
-    return {};
+    const internal = ref({
+      name: ''
+    });
+    const rules = ref({
+      required: v => !!v || 'required'
+    });
+
+    const external = ref({
+      name: ''
+    });
+
+    return {
+      internal,
+      rules,
+      external
+    };
   }
 });
 </script>
